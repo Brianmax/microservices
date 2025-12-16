1db74008b771d23f00c9b1de7568d8eb2ee5704f
george <george.maxi@ucsp.edu.pe>
2025-12-15 20:19:33 -0500

    ms-2

---
+++ ms-2/src/main/java/org/example/ms2/controller/BasicController.java
@@ -0,0 +1,18 @@
+package org.example.ms2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/basic")
+public class BasicController {
+
+    @GetMapping("/aprobado")
+    public String isAprobado(float nota) {
+        if (nota >= 13) {
+            return "YES";
+        }
+        return "NO";
+    }
+}
